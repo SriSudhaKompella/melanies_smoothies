e68a1d1d3005cb9160ff3bff56da4a30d43c858f
SriSudhaKompella <158275992+SriSudhaKompella@users.noreply.github.com>
2025-04-10 17:44:10 +0530

    Update streamlit_app.py
    App update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col 
 
 # Write directly to the app
-# st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
-st.title(f"My Parents New Healthy Diner")
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
 st.write(
   """Choose the fruits you want in your custom Smoothie!.
   """
@@ -17,14 +16,16 @@ st.write(
 # option2= st.selectbox('What is your favorite fruit?',('Banana','Strawberries','Peaches'))
 # st.write('Your favorite fruit is :',option2)
 
-cnx=st.connection("snowflake")
-session=cnx.session()
-# session=get_active_session()
+
+session=get_active_session()
+
+name_on_order=st.text_input('Name on Smoothie:')
+# st.write('the name on your Smoothie will be:', name_on_order)
 
 my_dataframe=session.table("Smoothies.public.fruit_options").select(col('Fruit_name'))
 # st.dataframe(data=my_dataframe,use_container_width=True)
 
-ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe)
+ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
 
 if ingredients_list:
     # st.write(ingredients_list)
@@ -36,8 +37,8 @@ if ingredients_list:
 
     # st.write(ingredients_string)
 
-    my_insert_stmt=""" insert into smoothies.public.orders(ingredients)
-                        values ('"""+ ingredients_string+ """')
+    my_insert_stmt=""" insert into smoothies.public.orders(ingredients,name_on_order)
+                        values ('"""+ ingredients_string+ """','"""+name_on_order+"""')
                     """
     # st.write(my_insert_stmt)
     time_to_insert=st.button('Submit Order')
@@ -45,8 +46,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
-        st.success('Your Smoothie is ordered!',icon="✅")
-
+        st.success('Your Smoothie is ordered, '+name_on_order+'!',icon="✅")
+        
 
 
 
