0f1cf5960d455800c33d3558383da290a10c40ec
SriSudhaKompella <158275992+SriSudhaKompella@users.noreply.github.com>
2025-04-10 17:46:20 +0530

    Update streamlit_app.py
    Session update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col 
 
 # Write directly to the app
@@ -16,8 +16,9 @@ st.write(
 # option2= st.selectbox('What is your favorite fruit?',('Banana','Strawberries','Peaches'))
 # st.write('Your favorite fruit is :',option2)
 
-
-session=get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
+# session=get_active_session()
 
 name_on_order=st.text_input('Name on Smoothie:')
 # st.write('the name on your Smoothie will be:', name_on_order)
