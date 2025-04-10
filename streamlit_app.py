298f01fb1ccc14d42f8ee9666a7456feda0cece0
SriSudhaKompella <158275992+SriSudhaKompella@users.noreply.github.com>
2025-04-10 18:33:57 +0530

    Update streamlit_app.py
    reverted changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,7 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!',icon="✅")
-        
-smoothiefroot_response2 = requests.get("https://my.smoothiefroot.com/api/fruit/all")
-st.write(smoothiefroot_response2.json())
+
 
 
 
