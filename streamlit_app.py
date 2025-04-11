714199d3719976f01d9e1538ee9f17162ea828f8
SriSudhaKompella <158275992+SriSudhaKompella@users.noreply.github.com>
2025-04-11 12:14:06 +0530

    Update streamlit_app.py
    Make a Version of my_dataframe, but call it pd_df

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,12 @@ name_on_order=st.text_input('Name on Smoothie:')
 # st.write('the name on your Smoothie will be:', name_on_order)
 
 my_dataframe=session.table("Smoothies.public.fruit_options").select(col('Fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe,use_container_width=True)
+# st.dataframe(data=my_dataframe,use_container_width=True)
+# st.stop()
+
+# convert the snowpark dataframe to pandas dataframe so we can use loc function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list=st.multiselect('Choose up to 5 ingredients:',my_dataframe,max_selections=5)
